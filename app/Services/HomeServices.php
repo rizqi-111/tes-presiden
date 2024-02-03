62a94756938b106cfa325afd1313c1bae73940a1
rizqi <arus.code@gmail.com>
2024-02-03 07:14:34 +0700

    add try catch

--- app/Services/HomeServices.php
+++ app/Services/HomeServices.php
@@ -2,19 +2,28 @@
 
 namespace App\Services;
 
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\Http;
 
 class HomeServices
 {
     public function getData(): array
     {
-        $get = Http::get("https://mocki.io/v1/92a1f2ef-bef2-4f84-8f06-1965f0fca1a7")->json();
+        try {
+            $get = Http::get("https://mocki.io/v1/92a1f2ef-bef2-4f84-8f06-1965f0fca1a7")->json();
 
-        $capres = $get['calon_presiden'];
-        $cawapres = $get['calon_wakil_presiden'];
+            $capres = $get['calon_presiden'];
+            $cawapres = $get['calon_wakil_presiden'];
 
-        usort($capres, function($a, $b) {return strcmp($a['nomor_urut'], $b['nomor_urut']);});
-        usort($cawapres, function($a, $b) {return strcmp($a['nomor_urut'], $b['nomor_urut']);});
+            usort($capres, function ($a, $b) {
+                return strcmp($a['nomor_urut'], $b['nomor_urut']);
+            });
+            usort($cawapres, function ($a, $b) {
+                return strcmp($a['nomor_urut'], $b['nomor_urut']);
+            });
+        } catch (RequestException $e) {
+            throw new ('Request timeout or Connection error');
+        }
 
         return [
             'capres' => $capres,
